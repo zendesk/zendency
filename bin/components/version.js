6f6ead2001bcd4ff91c214007db1461acae47619
Baldvin Mar Smarason <bsmarason@zendesk.com>
2017-02-20 23:00:45 -0800

    Remove bracket

--- bin/components/version.js
+++ bin/components/version.js
@@ -4,17 +4,15 @@ const fs  = require('fs')
 // Module
 module.exports = (manifest, output) => {
 
-    // Get relative path
-    const file = path.join(process.env.PWD, output)
+  // Get relative path
+  const file = path.join(process.env.PWD, output)
 
-    // Update manifest version
-    manifest.version = current.slice(1)
+  // Update manifest version
+  manifest.version = current.slice(1)
 
-    // Overwrite manifest
-    fs.writeFile(file, JSON.stringify(manifest, null, 2), error => {
-      if (error) throw err
-    });
-
-  }
+  // Overwrite manifest
+  fs.writeFile(file, JSON.stringify(manifest, null, 2), error => {
+    if (error) throw err
+  })
 
 }
